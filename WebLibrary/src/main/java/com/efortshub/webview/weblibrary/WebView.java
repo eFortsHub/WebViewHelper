35f8c8097d680384ab1c87c1c89908a608e197ba
H. Bappi <contact.efortshub@gmail.com>
2021-05-03 23:56:50 +0600

    H. Bappi- eFortsHub

--- WebLibrary/src/main/java/com/efortshub/webview/weblibrary/WebView.java
+++ WebLibrary/src/main/java/com/efortshub/webview/weblibrary/WebView.java
@@ -1,42 +1,28 @@
 package com.efortshub.webview.weblibrary;
 
 import android.content.Context;
+import android.os.Build;
 import android.util.AttributeSet;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 
 public class WebView extends android.webkit.WebView {
-    /**
-     * Construct a new WebView with a Context object.
-     *
-     * @param context A Context object used to access application assets.
-     */
+
     public WebView(@NonNull Context context) {
         super(context);
     }
 
-    /**
-     * Construct a new WebView with layout parameters.
-     *
-     * @param context A Context object used to access application assets.
-     * @param attrs   An AttributeSet passed to our parent.
-     */
     public WebView(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
 
-    /**
-     * Construct a new WebView with layout parameters and a default style.
-     *
-     * @param context      A Context object used to access application assets.
-     * @param attrs        An AttributeSet passed to our parent.
-     * @param defStyleAttr
-     */
     public WebView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public WebView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
--- app/src/main/java/com/efortshub/webview/library/MainActivity.java
+++ app/src/main/java/com/efortshub/webview/library/MainActivity.java
@@ -3,6 +3,7 @@ package com.efortshub.webview.library;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
 
 
